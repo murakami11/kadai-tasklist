1bb52d48d0cda9a7efc6567e7cf45a745b55ac0a
taro.kozima <p4vg123@gmail.com>
2019-04-22 13:03:46 +0000

    kadai2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @tasks = @user.tasks.order('created_at DESC').page(params[:page])
     counts(@user)
   end
 
